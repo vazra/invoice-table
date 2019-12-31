9da1c09136b645be18f2dcd04f118d15e2112253
Ameen Rashad <ameenrashadv@gmail.com>
2019-12-31 16:37:45 +0530

    CRA version

---
+++ styleguide.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "Invoice Table : react component for tables like in invoices",
+  components: "src/lib/index.js",
+  defaultExample: true,
+  usageMode: "expand",
+  showSidebar: false
+};
